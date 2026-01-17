2030d5a48418b5d4084758e67a20f904e5c59194
Blessing Tembo <temboblessing05@gmail.com>
2026-01-17 10:43:12 +0200

    Add orders page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LandingPage } from "./pages/Landing/LandingPage";
 import { HomePage } from "./pages/Home/HomePage"
 import { ItemPage } from "./pages/Item/ItemPage";
 import { CheckoutPage } from "./pages/Checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="home" element={<HomePage />} />
         <Route path="item" element={<ItemPage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={< OrdersPage /> } />
       </Routes>
      
      
---
+++ src/pages/orders/OrdersHeader.jsx
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs';
+import { formartCurrency } from '../../utils/money.js';
+
+export function OrdersHeader({ order }) {
+  return (
+
+    <div className="order-header">
+      <div className="order-header-left-section">
+        <div className="order-date">
+          <div className="order-header-label">Order Placed:</div>
+          <div>
+            {dayjs(order.orderTimeMs).format('MMMM D ')}
+          </div>
+        </div>
+        <div className="order-total">
+          <div className="order-header-label">Total:</div>
+          <div>
+            {formartCurrency(order.totalCostCents)}
+          </div>
+        </div>
+      </div>
+
+      <div className="order-header-right-section">
+        <div className="order-header-label">Order ID:</div>
+        <div>
+          {order.id}
+        </div>
+      </div>
+    </div>
+  );
+}
