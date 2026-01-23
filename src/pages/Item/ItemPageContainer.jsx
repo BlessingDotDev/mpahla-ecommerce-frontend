eb3a8b17168143f13c48430d1c9ef8fb93d96f37
Blessing Tembo <temboblessing05@gmail.com>
2026-01-23 10:31:12 +0200

    Make add to cart button interactive

--- src/pages/Item/ItemPageContainer.jsx
+++ src/pages/Item/ItemPageContainer.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
 import axios from "axios";
+import { useParams } from "react-router";
 import { formatCurrency } from "../../scripts/utils/money.js";
 import "./ItemPageContainer.scss";
 import { Loading } from "../../components/Loading";
 
 export function ItemPageContainer() {
   const [product, setProduct] = useState(null);
-
   const { id } = useParams();
 
   useEffect(() => {
@@ -27,66 +26,79 @@ export function ItemPageContainer() {
     return (
       <div className="main-item-container js-main-container">
   
-        <div class="product-names display-product-names">
-          <p class="product-brand">
+        <div className="product-names display-product-names">
+          <p className="product-brand">
             {product.brand}
           </p>
-          <p class="product-name">
+          <p className="product-name">
             {product.name}
           </p>
-          <p class="product-price">
-            R{formatCurrency(product.priceCents)}
+          <p className="product-price">
+            {formatCurrency(product.priceCents)}
           </p>
         </div>
   
-        <button class="add-to-cart-button display-add-top-button js-add-to-cart-button"
+        <button className="add-to-cart-button display-add-top-button js-add-to-cart-button"
           data-product-id={product._id}
+          onClick={async () => {
+          try {
+            const res = await axios.post('/api/cart', {
+              product: product._id, 
+              quantity: 1,
+              deliveryOption: '6971e3c1535c8bfb31e97d7c'
+            });
+
+            console.log('Added to cart:', res.data);
+          } catch (err) {
+            console.error('Add to cart failed:', err.response?.data || err.message);
+          }
+        }}
         >
           Add to Cart
         </button>
   
-        <div class="image-container">
-          <img class="product-image" src={product.image} alt="product-image"/>
+        <div className="image-container">
+          <img className="product-image" src={product.image} alt="product-image"/>
         </div>
   
-        <div class="product-info">
-          <div class="product-names">
-            <p class="product-brand">
+        <div className="product-info">
+          <div className="product-names">
+            <p className="product-brand">
               {product.brand}
             </p>
-            <p class="product-name">
+            <p className="product-name">
               {product.name}
             </p>
-            <p class="product-price">
+            <p className="product-price">
               R{formatCurrency(product.priceCents)}
             </p>
           </div>
   
-          <div class="size-section">
-            <div class="size-title">
-              <span class="select-size primary-size">
+          <div className="size-section">
+            <div className="size-title">
+              <span className="select-size primary-size">
                 Select Size
               </span>
   
-              <div class="guide-container">
-                <img class="guide-icon" src="images/icons/arrow-down-circle.svg" alt=""/>
-                  <span class="size-guide primary-size">
+              <div className="guide-container">
+                <img className="guide-icon" src="images/icons/arrow-down-circle.svg" alt=""/>
+                  <span className="size-guide primary-size">
                     Size Guide
                   </span>
               </div>
             </div>
   
-            <ul class="size-options">
-              <li class="js-size-button">XS</li>
-              <li class="js-size-button">S</li>
-              <li class="js-size-button">M</li>
-              <li class="js-size-button">L</li>
-              <li class="js-size-button">XL</li>
-              <li class="js-size-button">XXL</li>
+            <ul className="size-options">
+              <li className="js-size-button">XS</li>
+              <li className="js-size-button">S</li>
+              <li className="js-size-button">M</li>
+              <li className="js-size-button">L</li>
+              <li className="js-size-button">XL</li>
+              <li className="js-size-button">XXL</li>
             </ul>
   
-            <div class="added-row">
-              <select name="number" id="number" class="js-select-value-${product.id}">
+            <div className="added-row">
+              <select name="number" id="number" className="js-select-value-${product.id}">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -99,26 +111,39 @@ export function ItemPageContainer() {
                 <option value="10">10</option>
               </select>
   
-              <span class="added-message js-added-message-${product.id}">Added</span>
+              <span className={`added-message js-added-message-${product.id}`}>Added</span>
             </div>
   
-            <button class="add-to-cart-button js-add-to-cart-button"
-              data-product-id="${product.id}"
+            <button className="add-to-cart-button js-add-to-cart-button"
+              data-product-id={product._id}
+              onClick={async () => {
+              try {
+                const res = await axios.post('/api/cart', {
+                  product: product._id, 
+                  quantity: 1,
+                  deliveryOption: '6971e3c1535c8bfb31e97d7c'
+                });
+
+                console.log('Added to cart:', res.data);
+              } catch (err) {
+                console.error('Add to cart failed:', err.response?.data || err.message);
+              }
+            }}
             >
               Add to Cart
             </button>
   
-            <button class="favourite-button js-favourite-button">
-              Favourite <span class="heart js-heart">&#9825;</span>
+            <button className="favourite-button js-favourite-button">
+              Favourite <span className="heart js-heart">&#9825;</span>
             </button>
   
-            <div class="ratings">
-              <p class="title-ratings">Ratings</p>
-              <div class="stars-row">
-                Stars: <img class="rating-image" src={`images/ratings/rating-${product.ratings.starts * 10}.png`} alt="" />
+            <div className="ratings">
+              <p className="title-ratings">Ratings</p>
+              <div className="stars-row">
+                Stars: <img className="rating-image" src={`images/ratings/rating-${product.ratings.starts * 10}.png`} alt="" />
               </div>
-              <div class="counts-row">
-                Counts: <span class="count">{product.ratings.counts}</span>
+              <div className="counts-row">
+                Counts: <span className="count">{product.ratings.counts}</span>
               </div>
             </div>
           </div>
