f8426506501c78bfb5f7c5dd92734e3d5ebdae16
Blessing Tembo <temboblessing05@gmail.com>
2026-01-24 17:13:54 +0200

    Change home styles

--- src/pages/Home/HomePageGrid.jsx
+++ src/pages/Home/HomePageGrid.jsx
@@ -42,36 +42,10 @@ export function HomePageGrid({ products, setProducts }) {
                 <p className="product-brand">
                   Nike-Jordan
                 </p>
-                <div className="product-ratings">
-                  <img className="rating-image" src={`/images/ratings/rating-${product.ratings.starts * 10}.png`} alt="" />
-                  <span className="rating-count">
-                    {product.ratings.counts}
-                  </span>
-                </div>
                 <p className="product-price">
                   R{formatCurrency(product.priceCents)}
                 </p>
 
-                <div className="added-row">
-                  <select name="number" id="number" className="js-select-value-${product._id}">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-
-                  <span className={`added-message js-added-message-${product._id}`}>Added</span>
-                </div>
-
-                <button className="add-to-cart-button js-add-to-cart-button" data-product-id={product._id}>
-                  Add to cart
-                </button>
               </div>
             </div>
           )
